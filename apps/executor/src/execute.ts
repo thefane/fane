f052ef06cf1ac659f5e34f03d3e2152c00efb8d6
shivam-jainn <shivam.clgstash@gmail.com>
2023-11-15 03:48:19 -0500

    made execute function , copyfile functions

--- apps/executor/src/execute.ts
+++ apps/executor/src/execute.ts
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+import { error } from "console";
 
 enum Languages{
     python,
@@ -21,17 +22,40 @@ function placePredefinedFiles(problemNumber:Number,language:Languages){
     // send user code and predefined to sandbox/files
 }
 
-function placeUserCode(userCode:File){
+function placeUserCode(userCode:File,language:Languages){
     // place user code file in sandbox/files
+    exec(`mv usercode.${language}`,(output,error)=>{
+        try {
+            console.log("Code done ");
+            return {"result":"pass","timetaken":1,"spacetaken":2}
+        } catch (error) {
+            console.log(error);
+            
+        }
+    });
 }
 
 function executeCode(userCode:File,problemNumber:Number,language:Languages) {
     placePredefinedFiles(problemNumber,language);
     placeUserCode(userCode);
+    const [result, timetaken, spacetaken] = execcommand();
+
+    return {result,timetaken,spacetaken}
 }
 
 function execcommand(){
     // use exec from child_process
-    exec('docker')
+    exec('docker --rm run faneexec ',(output,error)=>{
+        try {
+            console.log("Code done ");
+            return {"result":"pass","timetaken":1,"spacetaken":2}
+        } catch (error) {
+            console.log(error);
+            
+        }
+    });
     // docker run rm faneexec 
 }
+
+
+export default executeCode;
--- apps/executor/src/server.ts
+++ apps/executor/src/server.ts
@@ -1,12 +1,21 @@
 import express,{ Express,Request,Response } from "express";
+import executeCode from "./execute";
 
 const server:Express = express();
 const PORT:Number = 3000;
 
-server.post('/submission',(req:Request,res:Response)=>{
-    const userCode = req.body;
+server.use(express.json());
 
-    
-})
+server.post('/submission', async (req: Request, res: Response) => {
+    const { userCode, problemNumber, language, userid } = req.body;
+
+    const [result:String, timetaken:Number, spacetaken:Number] = await executeCode(userCode, problemNumber, language);
+
+    res.send({
+        result,
+        timetaken,
+        spacetaken
+    });
+});
 
 
